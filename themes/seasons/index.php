5c2096b9baf0688064f0624b301fe30951e3213f
Kevin Martínez <n4utilius@gmail.com>
2020-09-30 04:16:50 -0500

    Integración de entrada

--- themes/seasons/index.php
+++ themes/seasons/index.php
@@ -1,54 +1,274 @@
-<?php echo head(array('bodyid'=>'home')); ?>
+<!doctype html>
+<html class="no-js" lang="zxx">
+<head>
 
-<?php if (get_theme_option('Homepage Text')): ?>
-<p><?php echo get_theme_option('Homepage Text'); ?></p>
-<?php endif; ?>
+    <style type="text/css">
+        .enable{ display: none;}
+        .able{ display: inline-block;}
+    </style>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title> Biblioteca Fudar</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="/themes/seasons/assets/img/favicon.ico">
 
-<?php if (get_theme_option('Display Featured Item') !== '0'): ?>
-<!-- Featured Item -->
-<div id="featured-item">
-    <h2><?php echo __('Featured Item'); ?></h2>
-    <?php echo random_featured_items(1, true); ?>
-    <?php //echo random_featured_items(1); ?>
-</div><!--end featured-item-->
-<?php endif; ?>
+    <!-- CSS here -->
+    <link rel="stylesheet" href="/themes/seasons/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/themes/seasons/assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/themes/seasons/assets/css/slicknav.css">
+    <link rel="stylesheet" href="/themes/seasons/assets/css/flaticon.css">
+    <link rel="stylesheet" href="/themes/seasons/assets/css/progressbar_barfiller.css">
+    <link rel="stylesheet" href="/themes/seasons/assets/css/gijgo.css">
+    <link rel="stylesheet" href="/themes/seasons/assets/css/animate.min.css">
+    <link rel="stylesheet" href="/themes/seasons/assets/css/animated-headline.css">
+    <link rel="stylesheet" href="/themes/seasons/assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="/themes/seasons/assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="/themes/seasons/assets/css/themify-icons.css">
+    <link rel="stylesheet" href="/themes/seasons/assets/css/slick.css">
+    <link rel="stylesheet" href="/themes/seasons/assets/css/nice-select.css">
+    <link rel="stylesheet" href="/themes/seasons/assets/css/style.css">
+    
+</head>
+<body>
 
-<?php fire_plugin_hook('public_content_top', array('view'=>$this)); ?>
+        <!-- ? Preloader Start -->
+        <div id="preloader-active">
+            <div class="preloader d-flex align-items-center justify-content-center">
+                <div class="preloader-inner position-relative">
+                    <div class="preloader-circle"></div>
+                    <div class="preloader-img pere-text">
+                        <img src="/themes/seasons/assets/img/logo/loder.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Preloader Start -->
+    <section id="welcome">
+        <header>
+            <!-- Header Start -->
+            <div class="header-area header-transparent">
+                <div class="main-header ">
+                    <div class="header-bottom">
+                        <div class="container-fluid">
+                            <div class="row align-items-center">
+                                <!-- Logo -->
+                                <div class="col-xl-2 col-lg-2">
+                                    <div class="logo">
+                                        <img src="/themes/seasons/assets/img/logo/Logo-Fundar.png" style="padding: 30px;" alt="Biblioteca">
+                                    
+                                    </div>
+                                </div>
+                                <div class="col-xl-10 col-lg-10">
+                                    <div class="menu-wrapper d-flex align-items-center justify-content-end">
+                                        <!-- Main-menu -->
+                                        <div class="main-menu d-none d-lg-block">
 
-<?php if (get_theme_option('Display Featured Collection') !== '0'): ?>
-<!-- Featured Collection -->
-<div id="featured-collection">
-    <h2><?php echo __('Featured Collection'); ?></h2>
-    <?php echo random_featured_collection(); ?>
-</div><!-- end featured collection -->
-<?php endif; ?>
+                                        </div>
+                                    </div>
+                                </div> 
+                                <!-- Mobile Menu -->
+                                <div class="col-12">
+                                    <div class="mobile_menu d-block d-lg-none"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Header End -->
+        </header>
+        <main>
+            <!-- Slider Area Start-->
+            <div class="slider-area slider-bg ">
+                <div class="slider-active">
+                    <!-- Single Slider -->
+                    <div class="single-slider d-flex align-items-center slider-height ">
+                        <div class="container">
+                            <div class="row align-items-center justify-content-between">
+                                <div class="col-xl-5 col-lg-5 col-md-9 ">
+                                    <div class="hero__caption">
+                                       
+                                        <h1 data-animation="fadeInLeft" data-delay=".6s ">Biblioteca</h1>
+                                        <p data-animation="fadeInLeft" data-delay=".8s">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                                        <!-- Slider btn -->
+                                        <div class="slider-btns">
+                                            <!-- Hero-btn -->
+                                            <a data-animation="fadeInLeft" id="mostrar_home" data-delay="1s" href="http://biblioteca.fundarlabs.mx/" class="btn radius-btn">Entrar</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6">
+                                    <div class="hero__img d-none d-lg-block f-right">
+                                        <img src="/themes/seasons/assets/img/hero/hero_right.png" alt="" data-animation="fadeInRight" data-delay="1s">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>      
+                    <!-- Single Slider -->
+                    <div class="single-slider d-flex align-items-center slider-height ">
+                        <div class="container">
+                            <div class="row align-items-center">
+                                <div class="col-lg-6 col-md-9 ">
+                                    <div class="hero__caption">
+                                        <span data-animation="fadeInLeft" data-delay=".3s">Best Domain & hosting service provider</span>
+                                        <h1 data-animation="fadeInLeft" data-delay=".6s">Powerful web hosting</h1>
+                                        <p data-animation="fadeInLeft" data-delay=".8s">Supercharge your WordPress hosting with detailed 
+                                            website analytics, marketing tools, security, and data 
+                                        backups all in one place.</p>
+                                        <!-- Slider btn -->
+                                        <div class="slider-btns">
+                                            <!-- Hero-btn -->
+                                            <a data-animation="fadeInLeft" data-delay="1s" href="/industries.html" class="btn radius-btn">get started</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="hero__img d-none d-lg-block f-right">
+                                        <img src="/themes/seasons/assets/img/hero/hero_right.png" alt="" data-animation="fadeInRight" data-delay="1s">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>      
+                </div>
+                <!-- Slider Shape -->
+                <div class="slider-shape d-none d-lg-block">
+                    <img class="slider-shape1" src="/themes/seasons/assets/img/hero/top-left-shape.png" alt="">
+                </div>
+            </div>
+            </div>
+        </main>
+        <footer style="display: none;">
+         
+                <!-- footer-bottom area -->
+                <div class="footer-bottom-area">
+                    <div class="container">
+                        <div class="footer-border">
+                            <div class="row">
+                                <div class="col-xl-12">
+                                    <div class="footer-copy-right text-center">
+                                        <p><
+                                          Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="">FundarLab</a>
+                                         </p>
 
-<?php if ((get_theme_option('Display Featured Exhibit') !== '0')
-        && plugin_is_active('ExhibitBuilder')
-        && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
-<!-- Featured Exhibit -->
-<?php echo exhibit_builder_display_random_featured_exhibit(); ?>
-<?php endif; ?>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </footer>
+    </section>
 
-<?php
-$recentItems = get_theme_option('Homepage Recent Items');
-if ($recentItems === null || $recentItems === ''):
-    $recentItems = 10;
-else:
-    $recentItems = (int) $recentItems;
-endif;
-if ($recentItems):
-?>
-<div id="recent-items">
-    <h2><?php echo __('Recently Added Items'); ?></h2>
-    <?php echo recent_items($recentItems); ?>
-    <p class="view-items-link"><a href="<?php echo html_escape(url('items')); ?>"><?php echo __('View All Items'); ?></a></p>
-</div><!--end recent-items -->
-<?php endif; ?>
+    <!-- JS here -->
 
-<?php fire_plugin_hook('public_home', array('view' => $this)); ?>
+    <script src="/themes/seasons/assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    <!-- Jquery, Popper, Bootstrap -->
+    <script src="/themes/seasons/assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="/themes/seasons/assets/js/popper.min.js"></script>
+    <script src="/themes/seasons/assets/js/bootstrap.min.js"></script>
+    <!-- Jquery Mobile Menu -->
+    <script src="/themes/seasons/assets/js/jquery.slicknav.min.js"></script>
 
-<?php echo foot(); ?>
+    <!-- Jquery Slick , Owl-Carousel Plugins -->
+    <script src="/themes/seasons/assets/js/owl.carousel.min.js"></script>
+    <script src="/themes/seasons/assets/js/slick.min.js"></script>
+    <!-- One Page, Animated-HeadLin -->
+    <script src="/themes/seasons/assets/js/wow.min.js"></script>
+    <script src="/themes/seasons/assets/js/animated.headline.js"></script>
+    <script src="/themes/seasons/assets/js/jquery.magnific-popup.js"></script>
+
+    <!-- Date Picker -->
+    <script src="/themes/seasons/assets/js/gijgo.min.js"></script>
+
+    <!-- Video bg -->
+    <script src="/themes/seasons/assets/js/jquery.vide.js"></script>
+
+    <!-- Nice-select, sticky -->
+    <script src="/themes/seasons/assets/js/jquery.nice-select.min.js"></script>
+    <script src="/themes/seasons/assets/js/jquery.sticky.js"></script>
+    <!-- Progress -->
+    <script src="/themes/seasons/assets/js/jquery.barfiller.js"></script>
+
+    <!-- counter , waypoint,Hover Direction -->
+    <script src="/themes/seasons/assets/js/jquery.counterup.min.js"></script>
+    <script src="/themes/seasons/assets/js/waypoints.min.js"></script>
+    <script src="/themes/seasons/assets/js/jquery.countdown.min.js"></script>
+    <script src="/themes/seasons/assets/js/hover-direction-snake.min.js"></script>
+
+    <!-- contact js -->
+    <script src="/themes/seasons/assets/js/contact.js"></script>
+    <script src="/themes/seasons/assets/js/jquery.form.js"></script>
+    <script src="/themes/seasons/assets/js/jquery.validate.min.js"></script>
+    <script src="/themes/seasons/assets/js/mail-script.js"></script>
+    <script src="/themes/seasons/assets/js/jquery.ajaxchimp.min.js"></script>
+
+    <!-- Jquery Plugins, main Jquery -->    
+    <script src="/themes/seasons/assets/js/plugins.js"></script>
+    <script src="/themes/seasons/assets/js/main.js"></script>
+
+    </body>
+    </html>
+
+<!-- Ola k ase -->
+
+<section id="home" class="enable">
+    <?php echo head(array('bodyid'=>'home')); ?>
+
+    <?php if (get_theme_option('Homepage Text')): ?>
+    <p><?php echo get_theme_option('Homepage Text'); ?></p>
+    <?php endif; ?>
+
+    <?php if (get_theme_option('Display Featured Item') !== '0'): ?>
+    <!-- Featured Item -->
+    <div id="featured-item">
+        <h2><?php echo __('Featured Item'); ?></h2>
+        <?php echo random_featured_items(1, true); ?>
+        <?php //echo random_featured_items(1); ?>
+    </div><!--end featured-item-->
+    <?php endif; ?>
+
+    <?php fire_plugin_hook('public_content_top', array('view'=>$this)); ?>
+
+    <?php if (get_theme_option('Display Featured Collection') !== '0'): ?>
+    <!-- Featured Collection -->
+    <div id="featured-collection">
+        <h2><?php echo __('Featured Collection'); ?></h2>
+        <?php echo random_featured_collection(); ?>
+    </div><!-- end featured collection -->
+    <?php endif; ?>
+
+    <?php if ((get_theme_option('Display Featured Exhibit') !== '0')
+            && plugin_is_active('ExhibitBuilder')
+            && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
+    <!-- Featured Exhibit -->
+    <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
+    <?php endif; ?>
+
+    <?php
+    $recentItems = get_theme_option('Homepage Recent Items');
+    if ($recentItems === null || $recentItems === ''):
+        $recentItems = 10;
+    else:
+        $recentItems = (int) $recentItems;
+    endif;
+    if ($recentItems):
+    ?>
+    <div id="recent-items">
+        <h2><?php echo __('Recently Added Items'); ?></h2>
+        <?php echo recent_items($recentItems); ?>
+        <p class="view-items-link"><a href="<?php echo html_escape(url('items')); ?>"><?php echo __('View All Items'); ?></a></p>
+    </div><!--end recent-items -->
+    <?php endif; ?>
+
+    <?php fire_plugin_hook('public_home', array('view' => $this)); ?>
+
+    <?php echo foot(); ?>
+</section>
 
 <script>
 $(document).ready(function(){
@@ -58,6 +278,21 @@ $(document).ready(function(){
 		var data = { url: cont.children("h3").children("a").attr("href"), text: cont.children("h3").children("a").text() }
 		cont.append('<a href="' + data.url + '" class="image"><img src="http://biblioteca.fundarlabs.org.mx/files/square_thumbnails/book-default.jpeg" alt="' + data.text + '" title="' + data.text + '" ></a>')
 	}
+
+    $("a#mostrar_home").on("click", function(e){
+        $("section#welcome").addClass("enable")
+        $("section#home").removeClass("enable")
+
+        $("html, body").animate({ scrollTop: 0 }, "fast");
+        $("#preloader-active").css("display", "block")
+
+        setTimeout(function(){ 
+            $("#preloader-active").css("display", "none")
+            $("section#home").fadeIn("slow") 
+        }, 1500);
+
+        return false;
+    })
 })
 </script>
 
