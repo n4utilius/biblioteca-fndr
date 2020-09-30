2de13002f1f76c3456704c54a84dfe48e0cf0202
Kevin Martínez <n4utilius@gmail.com>
2020-09-30 10:19:11 -0500

    Regresando código

--- themes/seasons/index.php
+++ themes/seasons/index.php
@@ -39,21 +39,22 @@
 </head>
 <body>
 
-        <!-- ? Preloader Start -->
-        <div id="preloader-active">
-            <div class="preloader d-flex align-items-center justify-content-center">
-                <div class="preloader-inner position-relative">
-                    <div class="preloader-circle"></div>
-                    <div class="preloader-img pere-text">
-                        <img src="/themes/seasons/assets/img/logo/loder.png" alt="">
-                    </div>
+    <!-- ? Preloader Start -->
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="/themes/seasons/assets/img/logo/loder.png" alt="">
                 </div>
             </div>
         </div>
-        <!-- Preloader Start -->
+    </div>
+
+    <!-- Preloader Start -->
     <section id="welcome">
+        <!-- Header Start -->
         <header>
-            <!-- Header Start -->
             <div class="header-area header-transparent">
                 <div class="main-header ">
                     <div class="header-bottom">
@@ -103,65 +104,36 @@
                                         <!-- Slider btn -->
                                         <div class="slider-btns">
                                             <!-- Hero-btn -->
-                                            <a data-animation="fadeInLeft" id="mostrar_home" data-delay="1s" href="http://biblioteca.fundarlabs.mx/" class="btn radius-btn">Entrar</a>
+                                            <a data-animation="fadeInLeft" data-delay="1s" id="mostrar_home" href="http://biblioteca.fundarlabs.mx/" class="btn radius-btn">Entrar</a>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-xl-6 col-lg-6">
                                     <div class="hero__img d-none d-lg-block f-right">
-                                        <img src="/themes/seasons/assets/img/hero/hero_right.png" alt="" data-animation="fadeInRight" data-delay="1s">
+                                        <img src="/themes/seasons/assets/img/hero/hero_gif-700px.gif" alt="" data-animation="fadeInRight" data-delay="1s">
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>      
-                    <!-- Single Slider -->
-                    <div class="single-slider d-flex align-items-center slider-height ">
-                        <div class="container">
-                            <div class="row align-items-center">
-                                <div class="col-lg-6 col-md-9 ">
-                                    <div class="hero__caption">
-                                        <span data-animation="fadeInLeft" data-delay=".3s">Best Domain & hosting service provider</span>
-                                        <h1 data-animation="fadeInLeft" data-delay=".6s">Powerful web hosting</h1>
-                                        <p data-animation="fadeInLeft" data-delay=".8s">Supercharge your WordPress hosting with detailed 
-                                            website analytics, marketing tools, security, and data 
-                                        backups all in one place.</p>
-                                        <!-- Slider btn -->
-                                        <div class="slider-btns">
-                                            <!-- Hero-btn -->
-                                            <a data-animation="fadeInLeft" data-delay="1s" href="/industries.html" class="btn radius-btn">get started</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6">
-                                    <div class="hero__img d-none d-lg-block f-right">
-                                        <img src="/themes/seasons/assets/img/hero/hero_right.png" alt="" data-animation="fadeInRight" data-delay="1s">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>      
-                </div>
                 <!-- Slider Shape -->
                 <div class="slider-shape d-none d-lg-block">
                     <img class="slider-shape1" src="/themes/seasons/assets/img/hero/top-left-shape.png" alt="">
                 </div>
             </div>
-            </div>
-        </main>
-        <footer style="display: none;">
          
+            <footer>
+     
                 <!-- footer-bottom area -->
                 <div class="footer-bottom-area">
                     <div class="container">
                         <div class="footer-border">
                             <div class="row">
                                 <div class="col-xl-12">
-                                    <div class="footer-copy-right text-center">
-                                        <p><
-                                          Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="">FundarLab</a>
-                                         </p>
-
+                                    <div class="footer-copy-right text-left">
+                                        <p> <a href="/themes/seasons/assets/img/Reglamento-de-uso.pdf" target="blank">Guía de uso</a>
+                                        </p>
+        
                                       </div>
                                   </div>
                               </div>
@@ -169,57 +141,60 @@
                       </div>
                   </div>
               </div>
-          </footer>
+            </footer>   
+
+            </div>
+        </main>
     </section>
 
     <!-- JS here -->
 
-    <script src="/themes/seasons/assets/js/vendor/modernizr-3.5.0.min.js"></script>
-    <!-- Jquery, Popper, Bootstrap -->
-    <script src="/themes/seasons/assets/js/vendor/jquery-1.12.4.min.js"></script>
-    <script src="/themes/seasons/assets/js/popper.min.js"></script>
-    <script src="/themes/seasons/assets/js/bootstrap.min.js"></script>
-    <!-- Jquery Mobile Menu -->
-    <script src="/themes/seasons/assets/js/jquery.slicknav.min.js"></script>
+        <script src="/themes/seasons/assets/js/vendor/modernizr-3.5.0.min.js"></script>
+        <!-- Jquery, Popper, Bootstrap -->
+        <script src="/themes/seasons/assets/js/vendor/jquery-1.12.4.min.js"></script>
+        <script src="/themes/seasons/assets/js/popper.min.js"></script>
+        <script src="/themes/seasons/assets/js/bootstrap.min.js"></script>
+        <!-- Jquery Mobile Menu -->
+        <script src="/themes/seasons/assets/js/jquery.slicknav.min.js"></script>
 
-    <!-- Jquery Slick , Owl-Carousel Plugins -->
-    <script src="/themes/seasons/assets/js/owl.carousel.min.js"></script>
-    <script src="/themes/seasons/assets/js/slick.min.js"></script>
-    <!-- One Page, Animated-HeadLin -->
-    <script src="/themes/seasons/assets/js/wow.min.js"></script>
-    <script src="/themes/seasons/assets/js/animated.headline.js"></script>
-    <script src="/themes/seasons/assets/js/jquery.magnific-popup.js"></script>
+        <!-- Jquery Slick , Owl-Carousel Plugins -->
+        <script src="/themes/seasons/assets/js/owl.carousel.min.js"></script>
+        <script src="/themes/seasons/assets/js/slick.min.js"></script>
+        <!-- One Page, Animated-HeadLin -->
+        <script src="/themes/seasons/assets/js/wow.min.js"></script>
+        <script src="/themes/seasons/assets/js/animated.headline.js"></script>
+        <script src="/themes/seasons/assets/js/jquery.magnific-popup.js"></script>
 
-    <!-- Date Picker -->
-    <script src="/themes/seasons/assets/js/gijgo.min.js"></script>
+        <!-- Date Picker -->
+        <script src="/themes/seasons/assets/js/gijgo.min.js"></script>
 
-    <!-- Video bg -->
-    <script src="/themes/seasons/assets/js/jquery.vide.js"></script>
+        <!-- Video bg -->
+        <script src="/themes/seasons/assets/js/jquery.vide.js"></script>
 
-    <!-- Nice-select, sticky -->
-    <script src="/themes/seasons/assets/js/jquery.nice-select.min.js"></script>
-    <script src="/themes/seasons/assets/js/jquery.sticky.js"></script>
-    <!-- Progress -->
-    <script src="/themes/seasons/assets/js/jquery.barfiller.js"></script>
+        <!-- Nice-select, sticky -->
+        <script src="/themes/seasons/assets/js/jquery.nice-select.min.js"></script>
+        <script src="/themes/seasons/assets/js/jquery.sticky.js"></script>
+        <!-- Progress -->
+        <script src="/themes/seasons/assets/js/jquery.barfiller.js"></script>
 
-    <!-- counter , waypoint,Hover Direction -->
-    <script src="/themes/seasons/assets/js/jquery.counterup.min.js"></script>
-    <script src="/themes/seasons/assets/js/waypoints.min.js"></script>
-    <script src="/themes/seasons/assets/js/jquery.countdown.min.js"></script>
-    <script src="/themes/seasons/assets/js/hover-direction-snake.min.js"></script>
+        <!-- counter , waypoint,Hover Direction -->
+        <script src="/themes/seasons/assets/js/jquery.counterup.min.js"></script>
+        <script src="/themes/seasons/assets/js/waypoints.min.js"></script>
+        <script src="/themes/seasons/assets/js/jquery.countdown.min.js"></script>
+        <script src="/themes/seasons/assets/js/hover-direction-snake.min.js"></script>
 
-    <!-- contact js -->
-    <script src="/themes/seasons/assets/js/contact.js"></script>
-    <script src="/themes/seasons/assets/js/jquery.form.js"></script>
-    <script src="/themes/seasons/assets/js/jquery.validate.min.js"></script>
-    <script src="/themes/seasons/assets/js/mail-script.js"></script>
-    <script src="/themes/seasons/assets/js/jquery.ajaxchimp.min.js"></script>
+        <!-- contact js -->
+        <script src="/themes/seasons/assets/js/contact.js"></script>
+        <script src="/themes/seasons/assets/js/jquery.form.js"></script>
+        <script src="/themes/seasons/assets/js/jquery.validate.min.js"></script>
+        <script src="/themes/seasons/assets/js/mail-script.js"></script>
+        <script src="/themes/seasons/assets/js/jquery.ajaxchimp.min.js"></script>
 
-    <!-- Jquery Plugins, main Jquery -->    
-    <script src="/themes/seasons/assets/js/plugins.js"></script>
-    <script src="/themes/seasons/assets/js/main.js"></script>
+        <!-- Jquery Plugins, main Jquery -->    
+        <script src="/themes/seasons/assets/js/plugins.js"></script>
+        <script src="/themes/seasons/assets/js/main.js"></script>
 
-    </body>
+        </body>
     </html>
 
 <!-- Ola k ase -->
